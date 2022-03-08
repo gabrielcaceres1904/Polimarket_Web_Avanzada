9c60727e6a9206d1989fd8abdb18c7051bc4baee
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-03-08 11:27:14 -0500

    update/ perfil

--- polimarket/src/app/rutas/Administrador/ruta-admin-perfil/ruta-admin-perfil.component.ts
+++ polimarket/src/app/rutas/Administrador/ruta-admin-perfil/ruta-admin-perfil.component.ts
@@ -10,51 +10,11 @@ import {baseControlInterface} from "../../../servicios/interfaces/app/base-contr
 })
 export class RutaAdminPerfilComponent implements OnInit {
 
-  idUsuario = -1
-  usuarioActual: UsuarioInterface = {} as UsuarioInterface
-
-  formGroup: FormGroup
-
-  campos = [
-    {
-      titulo: 'Nombre', nombre: 'nombre', tipo: 'text', placeholder: 'Ingresa tu nombre', requeridoM: 'El nombre es requerido', longitudM: ''
-    },
-    {
-      titulo: 'Apellido', nombre: 'apellido', tipo: 'text', placeholder: 'Ingresa tu apellido', requeridoM: 'El apellido es requerido', longitudM: ''
-    },
-    {
-      titulo: 'Dirección de domicilio', nombre: 'domicilio', tipo: 'text', placeholder: 'Ingresa la dirección de tu domicilio', requeridoM: 'La dirección de domicilio es requerida', longitudM: ''
-    },
-    {
-      titulo: 'Correo electrónico', nombre: 'email', tipo: 'text', placeholder: 'Ingresa tu correo electrónico', requeridoM: 'El correo electrónico es requerido', longitudM: ''
-    },
-  ] as baseControlInterface[]
-
   constructor() {
-    // @ts-ignore
-    const parametroRuta = this.activatedRoute.parent.params;
-    parametroRuta
-      .subscribe({
-        next:(parametrosRuta) => {
-          this.idUsuario = parametrosRuta['idCliente'];
-          this.buscarUsuario(this.idUsuario)
-        }
-      })
 
-    this.formGroup = this.formBuilder.group(
-      {
-        nombre: [this.usuarioActual ? this.usuarioActual.nombre : '', Validators.required],
-        apellido: [this.usuarioActual ? this.usuarioActual.apellido : '', Validators.required],
-        domicilio: [this.usuarioActual ? this.usuarioActual.direccion : '', Validators.required],
-        email: [this.usuarioActual.email ? this.usuarioActual.email: '', Validators.required],
-      }
-    )
   }
 
   ngOnInit(): void {
   }
 
-  actualizarUsuario() {
-
-  }
 }
