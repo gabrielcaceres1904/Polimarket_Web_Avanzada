c3fc96d1fd21b9e514559b3675df6da6999e2278
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-03-08 00:02:21 -0500

    update/ home dashboard del admin

--- polimarket/src/app/rutas/Administrador/ruta-home-admin/ruta-home-admin.component.ts
+++ polimarket/src/app/rutas/Administrador/ruta-home-admin/ruta-home-admin.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminHomeBoxInterface} from "../../../servicios/interfaces/app/admin-home-box.interface";
-import {SucursalProductoService} from "../../../servicios/http/sucursal-producto.service";
 import {SucursalService} from "../../../servicios/http/sucursal.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SucursalInterface} from "../../../servicios/interfaces/modelo/sucursal.interface";
@@ -13,97 +12,66 @@ import {UsuarioInterface} from "../../../servicios/interfaces/modelo/usuario.int
   styleUrls: ['./ruta-home-admin.component.scss']
 })
 export class RutaHomeAdminComponent implements OnInit {
-  tituloAdmin = 'Opciones de administrador';
+  tituloAdmin = '';
   nombreAdmin = ''
   categories = ['Home', 'Productos', "Pedidos"];
-  boxesAdminHome: AdminHomeBoxInterface[] = [
-    {
-      titulo: 'Pedidos sin stock',
-      icon: 'inventory_2',
-      value: -1
-    },
-    {
-      titulo: 'Pedidos entregados',
-      icon: 'local_shipping',
-      value: -1
-    },
-    {
-      titulo: 'Total de ingresos',
-      icon: 'payments',
-      value: -1
-    },
-  ]
   idUsuario = -1
-  sucursalAdmin: SucursalInterface = {} as SucursalInterface
 
-  constructor(private readonly sucursalProductoService: SucursalProductoService,
-              private readonly sucursalService: SucursalService,
+  constructor(private readonly sucursalService: SucursalService,
               private readonly activatedRoute: ActivatedRoute,
               private readonly router: Router,
               private readonly usuarioService: UsuarioService) {
 
   }
   ngOnInit() {
-
-    // @ts-ignore
-    const parametroRuta$ = this.activatedRoute.parent.params;
+    const parametroRuta$ = this.activatedRoute.params;
     parametroRuta$
       .subscribe({
         next:(parametrosRuta) => {
           //console.log(parametrosRuta)
           this.idUsuario = parametrosRuta['idAdmin'];
           this.buscarAdminSucursal(this.idUsuario)
-          this.buscarValoresHome()
           //console.log('Usuario Sidebar: ', this.idUsuario)
         }
       })
   }
 
   private buscarAdminSucursal(idUsuario: number) {
+    // Buscar info de admin
     this.usuarioService.buscarUno(idUsuario)
       .subscribe(
         {
           next: (datos) => { // try then
             const admin = datos as UsuarioInterface
+            this.nombreAdmin = admin.nombre + ' ' + admin.apellido
           },
           error: (error) => { // catch
             console.error({error});
           },
-        }
-      )
-
-    this.sucursalService.buscarTodos({})
-      .subscribe(
-        {
-          next: (datos) => { // try then
-            const sucursales = datos as SucursalInterface[]
-            for(let sucursal of sucursales){
-              if(sucursal.idUsuario == idUsuario){
-                this.sucursalAdmin = sucursal
-                this.tituloAdmin = sucursal.direccion
-              }
-            }
-          },
-          error: (error) => { // catch
-            console.error({error});
-          },
-        }
-      )
-  }
-
-  private buscarValoresHome() {
-    this.sucursalProductoService.buscarTodos({})
-      .subscribe(
-        {
-          next: (datos) => { // try then
-            const sucursalProductos = datos
-          },
-          error: (error) => { // catch
-            console.error({error});
-          },
+          complete: ()=> {
+            // Buscar info de sucursal
+            this.sucursalService.buscarTodos({})
+              .subscribe(
+                {
+                  next: (datos) => { // try then
+                    const sucursales = datos as SucursalInterface[]
+                    for(let sucursal of sucursales){
+                      if(sucursal.idUsuario == idUsuario){
+                        this.tituloAdmin = sucursal.direccion
+                      }
+                    }
+                  },
+                  error: (error) => { // catch
+                    console.error({error});
+                  },
+                  complete: () => {
+                    const ruta = ['/admin', this.idUsuario, 'home'];
+                    this.router.navigate(ruta);
+                  }
+                }
+              )
+          }
         }
       )
   }
-
-
 }
